0cc46dcbcc10f1dd7d6d62c5f4bfa316c9ffc289
stevenbachimont <stevenbachimont@gmail.com>
2024-05-16 14:52:36 +0200

    parralax3

--- src/pages/Jungle.jsx
+++ src/pages/Jungle.jsx
@@ -1,40 +1,11 @@
-import { useState, useEffect } from 'react';
-import './styles/Paralax.css';
 
-// eslint-disable-next-line react/prop-types
-const ImageComponent = ({ src, speed, scroll, direction }) => {
-    const [transform, setTransform] = useState(0);
-
-    useEffect(() => {
-        setTransform(scroll * speed);
-    }, [scroll, speed]);
+function Jungle() {
 
     return (
-        <img src={src} alt="" style={{ transform: `translateX(${direction * transform * 50}px) scale(${1 + transform / 1000})` }} />
+        <>
+            <h1>salut jungle</h1>
+        </>
     );
-};
+}
 
-const Jungle = () => {
-    const [scroll, setScroll] = useState(0);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setScroll(window.scrollY);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    return (
-        <div className="App">
-            <ImageComponent src="src/assets/g.png" speed={0.5} scroll={scroll} direction={-1} />
-            <ImageComponent src="src/assets/d.png" speed={0.5} scroll={scroll} direction={1} />
-        </div>
-    );
-};
-
-export default Jungle;
+export default Jungle
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,10 +1,40 @@
-function Welcome() {
+import { useState, useEffect } from 'react';
+import '../styles/Paralax.css';
+
+// eslint-disable-next-line react/prop-types
+const ImageComponent = ({ src, speed, scroll, direction }) => {
+    const [transform, setTransform] = useState(0);
+
+    useEffect(() => {
+        setTransform(scroll * speed);
+    }, [scroll, speed]);
 
     return (
-        <>
-            <h1>salut welcome</h1>
-        </>
+        <img src={src} alt="" style={{ transform: `translateX(${direction * transform * 50}px) scale(${1 + transform / 1000})` }} />
     );
-}
+};
 
-export default Welcome
+const Welcome = () => {
+    const [scroll, setScroll] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScroll(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <ImageComponent src="src/assets/g.png" speed={0.5} scroll={scroll} direction={-1} />
+            <ImageComponent src="src/assets/d.png" speed={0.5} scroll={scroll} direction={1} />
+        </div>
+    );
+};
+
+export default Welcome;
