b34a9d0f93dd56adc2bc10ac04ca9b13b07faeaf
Audren <audren.destin@gmail.com>
2024-05-16 17:17:34 +0200

    about en cours

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Jungle from "./pages/Jungle";
 import Welcome from "./pages/Welcome";
+import About from "./pages/About";
 
 const router = createBrowserRouter(
     [
@@ -16,6 +17,10 @@ const router = createBrowserRouter(
                 {
                     path: "jungle",
                     element: <Jungle />,
+                },
+                {
+                    path: "about",
+                    element: <About />,
                 }
 
             ]
