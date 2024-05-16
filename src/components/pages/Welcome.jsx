d3aee13766b485b0c3a04f6e83d23c9c4419cd98
stevenbachimont <stevenbachimont@gmail.com>
2024-05-16 19:27:15 +0200

    paralax-redirection

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -17,12 +17,30 @@ const ImageComponent = ({ src, speed, scroll, direction }) => {
     );
 };
 
+// eslint-disable-next-line react/prop-types
+const ImageComponent2 = ({ src, speed, scroll, direction }) => {
+    const [transform, setTransform] = useState(0);
+
+    useEffect(() => {
+        setTransform(scroll * speed);
+    }, [scroll, speed]);
+
+    return (
+        <img src={src} alt="" style={{ transform: `translateX(${direction * transform * 10}px) scale(${1 + transform / 30})` }} />
+    );
+};
+
 const Welcome = () => {
     const [scroll, setScroll] = useState(0);
 
     useEffect(() => {
         const handleScroll = () => {
             setScroll(window.scrollY);
+
+
+            if (window.scrollY > 900) {
+                window.location.href = '/jungle';
+            }
         };
 
         window.addEventListener('scroll', handleScroll);
@@ -34,8 +52,11 @@ const Welcome = () => {
 
     return (
         <>
-            <ImageComponent src="src/assets/g.png" speed={0.1} scroll={scroll} direction={-1} />
-            <ImageComponent src="src/assets/d.png" speed={0.1} scroll={scroll} direction={1} />
+            <ImageComponent2 src="src/assets/g2.png" speed={0.1} scroll={scroll} direction={-1} />
+            <ImageComponent2 src="src/assets/d2.png" speed={0.1} scroll={scroll} direction={1} />
+            <ImageComponent src="src/assets/g.png" speed={0.2} scroll={scroll} direction={-1} />
+            <ImageComponent src="src/assets/d.png" speed={0.2} scroll={scroll} direction={1} />
+
         </>
     );
 };
