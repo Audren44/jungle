ccda3db03fca658d34d0d9669c3b56141c031cbe
brewost <156411525+brewost@users.noreply.github.com>
2024-05-16 15:24:34 +0200

    add sound context

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,16 +1,5 @@
-import { useEffect } from 'react';
-
 function Welcome() {
-  useEffect(() => {
-    const music = new Audio('src/assets/sounds/jungle-noise.mp3');
-    music.loop = true; 
-    music.play();
 
-    return () => {
-      music.pause();
-      music.currentTime = 0; 
-    };
-  }, []); 
 
   return (
       <>
