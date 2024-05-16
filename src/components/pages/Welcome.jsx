f21c2663751598748c5e8982c92edeb45d3b9472
brewost <156411525+brewost@users.noreply.github.com>
2024-05-16 15:02:24 +0200

    add sound to welcome

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,9 +1,23 @@
-function Welcome() {
+import { useEffect } from 'react';
 
-    return (
-        <>
-        </>
-    );
+function Welcome() {
+  useEffect(() => {
+    const music = new Audio('src/assets/sounds/jungle-noise.mp3');
+    music.loop = true; 
+    music.play();
+
+    return () => {
+      music.pause();
+      music.currentTime = 0; 
+    };
+  }, []); 
+
+  return (
+      <>
+      </>
+  );
 }
 
-export default Welcome
+export default Welcome;
+
+
