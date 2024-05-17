e87cfda697fe5208b7cdc9778258a19dd65b1950
Michaël Couffignal <michael@couffignal.fr>
2024-05-17 09:47:31 +0200

    adding parrot img and sound

--- src/pages/Jungle.jsx
+++ src/pages/Jungle.jsx
@@ -1,21 +1,22 @@
-import soundParrot1 from "../../assets/sounds/parrotWTF.mp3"
-import parrot1 from "../../assets/parrot1.png"
-import parrotFly from "../../assets/parrotFly.jpg"
-import "../../global.css"
+import soundParrot1 from "../assets/sounds/parrotWTF.mp3"
+import parrot1 from "../assets/parrot1.png"
+import parrotFly from "../assets/parrotFly.jpg"
+import "../global.css"
 
 function Jungle() {
 
     return (
-        <>
+        <div className="jungle-global">
+
             <img className="parrot-one" src={parrot1} alt="parrot"></img>
-            <img className="parrot-one" src={parrotFly} alt="parrot flying"></img>
+            <img className="parrot-two" src={parrotFly} alt="parrot flying"></img>
             <p>test css , ici page Jungle</p>
             <audio controls>
                 <source src={soundParrot1} type="audio/mp3" />
                 Your browser does not support the audio element.
             </audio>
             <h1>salut jungle</h1>
-        </>
+        </div>
     );
 }
 
