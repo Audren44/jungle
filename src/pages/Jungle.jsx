cf09b03b559a6cb292d71b2641e47a8a3676211e
Michaël Couffignal <michael@couffignal.fr>
2024-05-17 15:39:25 +0200

    last commit

--- src/pages/Jungle.jsx
+++ src/pages/Jungle.jsx
@@ -13,26 +13,14 @@ import { useEffect } from "react";
 
 function Jungle() {
 
-    const parrotSoundAudio = new Audio(soundParrot1);
+    const parrotSoundAudio = [soundParrot1, soundParrot2, soundParrot3, soundParrot4];
     const playSoundParrot = () => {
-        parrotSoundAudio.play().catch(error => console.log(error));
-    };
-
-    const parrotSoundAudio2 = new Audio(soundParrot2);
-    const playSoundParrot2 = () => {
-        parrotSoundAudio2.play().catch(error => console.log(error));
-    };
-
-    const parrotSoundAudio3 = new Audio(soundParrot3);
-    const playSoundParrot3 = () => {
-        parrotSoundAudio3.play().catch(error => console.log(error));
-    };
-
-    const parrotSoundAudio4 = new Audio(soundParrot4);
-    const playSoundParrot4 = () => {
-        parrotSoundAudio4.play().catch(error => console.log(error));
-    };
-
+        const randomIndex = Math.floor(Math.random() * parrotSoundAudio.length);
+        const randomSound = parrotSoundAudio[randomIndex];
+        const audio = new Audio(randomSound);
+        audio.play().catch(error => console.log(error));
+        setSoundPlayed(true);
+    }
 
     //scroll back to home page*********************************
     // eslint-disable-next-line no-unused-vars
@@ -61,9 +49,9 @@ function Jungle() {
         <div className="jungle-global">
 
             <img className="parrot-one parrot-global div1 parrot-except" src={parrot1} alt="parrot" onClick={playSoundParrot}></img>
-            <img className="parrot-one parrot-global div2" src={parrot3} alt="parrot" onClick={playSoundParrot2}></img>
-            <img className="parrot-one parrot-global div3 parrot-except" src={parrot2} alt="parrot" onClick={playSoundParrot3}></img>
-            <img className="parrot-one parrot-global div4" src={parrot11} alt="parrot" onClick={playSoundParrot4}></img>
+            <img className="parrot-one parrot-global div2" src={parrot3} alt="parrot" onClick={playSoundParrot}></img>
+            <img className="parrot-one parrot-global div3 parrot-except" src={parrot2} alt="parrot" onClick={playSoundParrot}></img>
+            <img className="parrot-one parrot-global div4" src={parrot11} alt="parrot" onClick={playSoundParrot}></img>
             <img className="parrot-two parrot-global div5 translate" src={parrotFly} alt="parrot flying"></img>
 
         </div>
